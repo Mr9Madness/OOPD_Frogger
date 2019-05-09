c715ba96d54d16048f20e0d8f3fdb1069460f538
janwil <Testfase175084>
2019-05-09 14:30:58 +0200

    Sections hardcoded

--- src/main/java/nl/han/ica/Frogger/sections/RoadSection.java
+++ src/main/java/nl/han/ica/Frogger/sections/RoadSection.java
@@ -2,6 +2,8 @@ package nl.han.ica.Frogger.sections;
 
 import nl.han.ica.Frogger.Frogger;
 import nl.han.ica.Frogger.Objects.RoadObjects.Car;
+import nl.han.ica.Frogger.Objects.RoadObjects.FireCar;
+import nl.han.ica.Frogger.Objects.RoadObjects.Truck;
 import processing.core.PVector;
 
 public class RoadSection extends Section {
@@ -29,26 +31,13 @@ public class RoadSection extends Section {
         engine.addGameObject(new Car(engine,270,1),534,600); //LEFT
         engine.addGameObject(new Car(engine,270,1),720,600); //LEFT
 
-    //    engine.addGameObject(new FireCar(engine,90,2),260,500); //RIGHT
-  //      engine.addGameObject(new FireCar(engine,90,2),90,500); //RIGHT
-      //  engine.addGameObject(new FireCar(engine,90,2),560,500); //RIGHT
+        engine.addGameObject(new FireCar(engine,90,2),260,550); //RIGHT
+        engine.addGameObject(new FireCar(engine,90,2),90,550); //RIGHT
+        engine.addGameObject(new FireCar(engine,90,2),560,550); //RIGHT
 
-//        engine.addGameObject(new Truck(engine,270,1),0,690); //LEFT
- //       engine.addGameObject(new Truck(engine,270,1),334,690); //LEFT
-   //     engine.addGameObject(new Truck(engine,270,1),720,690); //LEFT
-
-//        engine.addGameObject(new Car(engine,90,1),0,pos.y + 50 - 60000); //RIGHT
-//        engine.addGameObject(new Truck(engine,90,1),500,pos.y); //RIGHT
-//        engine.addGameObject(new FireCar(engine,90,1),260,pos.y + 100); //RIGHT
-//
-//        engine.addGameObject(new Car(engine,270,1),engine.width,pos.y); //LEFT
-////        engine.addGameObject(new Car(engine,180,20),20,engine.height+20);
-//        engine.addGameObject(new Car(engine,270,10),70, engine.height);
-//        System.out.println("object spawned at " + engine.height+60 );
-//1
-        //    engine.addGameObject(new Car(engine,270,10),50,engine.height+10);
-
-      //  engine.addGameObject(new Car(engine,180,180),30,engine.height);
+        engine.addGameObject(new Truck(engine,270,1),0,500); //LEFT
+        engine.addGameObject(new Truck(engine,270,1),334,500); //LEFT
+        engine.addGameObject(new Truck(engine,270,1),720,500); //LEFT
     }
 
     @Override
